abb4c99e4a1c6b712124df280a5ea7ec793cd525
ali1234mmm <aligamegames999@gmail.com>
2021-09-15 10:01:31 +0300

    Created  the name porject  Structure

---
+++ css/problem2_2().py
@@ -0,0 +1,17 @@
+
+
+alist = ["a","e","i","o","u","y"]
+blist = ["alpha", "beta", "gamma", "delta", "epsilon", "eta", "theta"] 
+
+def problem2_2() :
+	print()
+	print([0])
+	print([-1])
+	print([3:5])
+	print([0:3])
+	print([3:])
+	print(len())
+	.append("z")
+	print()
+problem2_2()
+ 
